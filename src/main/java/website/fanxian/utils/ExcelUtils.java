d2927a9503da3a87a00b4b67cf5beee0989c1620
Kelvin <549652152@qq.com>
2018-12-12 17:51:43 +0800

    ExcelUtils

--- src/main/java/website/fanxian/utils/ExcelUtils.java
+++ src/main/java/website/fanxian/utils/ExcelUtils.java
@@ -2,32 +2,43 @@ package website.fanxian.utils;
 
 import com.supwisdom.spreadsheet.mapper.f2w.WorkbookReader;
 import com.supwisdom.spreadsheet.mapper.f2w.excel.Excel2WorkbookReader;
+import com.supwisdom.spreadsheet.mapper.model.core.Cell;
 import com.supwisdom.spreadsheet.mapper.model.core.Workbook;
 import com.supwisdom.spreadsheet.mapper.model.meta.*;
+import com.supwisdom.spreadsheet.mapper.model.msg.Message;
 import com.supwisdom.spreadsheet.mapper.o2w.DefaultObject2SheetComposer;
 import com.supwisdom.spreadsheet.mapper.o2w.DefaultObject2WorkbookComposer;
 import com.supwisdom.spreadsheet.mapper.o2w.Object2SheetComposer;
 import com.supwisdom.spreadsheet.mapper.o2w.Object2WorkbookComposer;
 import com.supwisdom.spreadsheet.mapper.validation.DefaultSheetValidationJob;
 import com.supwisdom.spreadsheet.mapper.validation.DefaultWorkbookValidationJob;
+import com.supwisdom.spreadsheet.mapper.validation.validator.cell.NumberValidator;
+import com.supwisdom.spreadsheet.mapper.validation.validator.cell.RequireValidator;
+import com.supwisdom.spreadsheet.mapper.validation.validator.workbook.SheetAmountValidator;
 import com.supwisdom.spreadsheet.mapper.w2f.WorkbookWriter;
 import com.supwisdom.spreadsheet.mapper.w2f.excel.Workbook2ExcelWriter;
 import com.supwisdom.spreadsheet.mapper.w2o.DefaultSheet2ObjectComposer;
 import com.supwisdom.spreadsheet.mapper.w2o.DefaultWorkbook2ObjectComposer;
 import com.supwisdom.spreadsheet.mapper.w2o.Sheet2ObjectComposer;
 import com.supwisdom.spreadsheet.mapper.w2o.Workbook2ObjectComposer;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.core.io.ByteArrayResource;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * ExcelUtils
  */
 public abstract class ExcelUtils {
+
+
+    //定义字段校验类型
+    public enum ImportExcelValidateTypeE {
+        REQUIRED,   //必填
+        NUMBER,//数值
+    }
+
 
     /**
      * 导出包含一个sheet的Excel工作薄为Resource对象
@@ -107,14 +118,15 @@ public abstract class ExcelUtils {
      * 导入Excel文件并转换成指定对象list
      * @param excelInputStream excel文件inputStream
      * @param ObjectPropNameList 要转换的VO类属性名列表
+     * @param validFieldMap 需要校验指定列的Map[K:要校验的字段名，V:校验类型]，无需校验传入null
      * @param clazz 要转换的VO类
      * @return
      */
-    public static <T> List<List<T>> importWorkBook(InputStream excelInputStream, List<String> ObjectPropNameList, Class<T> clazz) {
+    public static <T> List<List<T>> importWorkBook(InputStream excelInputStream, List<String> ObjectPropNameList, Map<String,ImportExcelValidateTypeE> validFieldMap, Class<T> clazz) {
         WorkbookReader workbookReader = new Excel2WorkbookReader();
         try (InputStream inputStream = excelInputStream) {
             Workbook workbook = workbookReader.read(inputStream);
-            return convertWBook2Object(workbook, ObjectPropNameList, clazz);
+            return convertWBook2Object(workbook, ObjectPropNameList, validFieldMap, clazz);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -122,11 +134,15 @@ public abstract class ExcelUtils {
     }
 
 
-    private static List convertWBook2Object(Workbook workbook, List<String> ObjectPropNameList, Class clazz) {
+    private static List convertWBook2Object(Workbook workbook, List<String> ObjectPropNameList, Map<String,ImportExcelValidateTypeE> validFieldMap, Class clazz) {
         if(workbook == null || ObjectPropNameList == null)
             return  null;
         DefaultWorkbookValidationJob workbookValidationJob = new DefaultWorkbookValidationJob();
         DefaultSheetValidationJob sheetValidationJob = new DefaultSheetValidationJob();
+        if(validFieldMap != null) {
+            workbookValidationJob.addValidator(new SheetAmountValidator(1));     //本方法暂时只校验1个sheet
+            workbookValidationJob.addSheetValidationJob(sheetValidationJob);
+        }
         WorkbookMeta workbookMeta = new WorkbookMetaBean();
         SheetMeta sheetMeta = new SheetMetaBean("sheet1", 2);
         int colNum = 1;
@@ -134,10 +150,36 @@ public abstract class ExcelUtils {
             FieldMeta fieldMeta = new FieldMetaBean(propName, colNum);
             sheetMeta.addFieldMeta(fieldMeta);
             //try validate start
+            if(validFieldMap != null) {
+                Object validTypeObj = validFieldMap.get(propName);
+                if(validTypeObj != null && validTypeObj instanceof ImportExcelValidateTypeE) {
+                    if(ImportExcelValidateTypeE.REQUIRED.equals(validTypeObj)) {
+                        sheetValidationJob.addValidator(new RequireValidator().matchField(propName).group(propName).errorMessage(propName+"字段列导入的数据不能有空值"));
+                    }else if(ImportExcelValidateTypeE.NUMBER.equals(validTypeObj)) {
+                        sheetValidationJob.addValidator(new NumberValidator(){
+                            @Override
+                            protected boolean doValidate(Cell cell, FieldMeta fieldMeta) {
+                                return NumberUtils.isNumber(cell.getValue());
+                            }
+                        }.matchField(propName).group(propName).errorMessage(propName+"字段列导入的数据必须是数值"));
+                    }
+                }
+            }
             //try validate end
             colNum++;
         }
         workbookMeta.addSheetMeta(sheetMeta);
+        //统一校验
+        if(validFieldMap != null) {
+            boolean valid = workbookValidationJob.validate(workbook, workbookMeta);
+            List<Message> workbookErrors = workbookValidationJob.getErrorMessages();
+            if (workbookErrors != null && !workbookErrors.isEmpty()) {
+                for (Message message : workbookErrors) {
+                    System.out.println("导入发生错误!" + "sheet:" + message.getSheetIndex() + "|col:" + message.getColumnIndex() + "|row:" + message.getRowIndex() + "|ErrorMsg:" + message.getMessage());
+                }
+                throw new RuntimeException("数据校验出错，导入Excel终止");
+            }
+        }
         Workbook2ObjectComposer workbook2ObjectComposer = new DefaultWorkbook2ObjectComposer();
         Sheet2ObjectComposer sheet2ObjectComposer = new DefaultSheet2ObjectComposer();
         sheet2ObjectComposer.setObjectFactory((row, sheet) -> {
